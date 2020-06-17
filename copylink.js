496e7e1817f75ad1c8339268f15ef8303d4c4b93
GrantSwajian <ggswajian5@gmail.com>
2020-06-17 10:09:29 -0700

    Update copylink.js

--- copylink.js
+++ copylink.js
@@ -1,7 +1,3 @@
-    function getURL() {
-        var link = window.location.href;
-        document.getElementById("pageLink").innerHTML = link;
-    }
-    function copyURL() {
+        function copyURL() {
         alert("The link " + window.location.href + " has been copied");
     }
