20ceafa05497404c11076d390b34eaefa17096b0
GrantSwajian <ggswajian5@gmail.com>
2020-06-17 09:29:31 -0700

    Update copylink.js

--- copylink.js
+++ copylink.js
@@ -1,4 +1,5 @@
     function getURL() {
         alert("The link " + window.location.href + " has been copied");
         var link = window.location.href;
+        document.getElementById("pageLink").innerHTML = link;
     }
